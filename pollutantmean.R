abd46e0047a1d60d5d998279015901bec0746ab5
Philip Brown <philipnbrown@gmail.com>
2015-09-14 20:40:11 -0600

    added pollutantmean file

---
+++ pollutantmean.R
@@ -0,0 +1,32 @@
+pollutantmean <- function(directory = "specdata", pollutant, id = 1:332) {
+    ## 'directory' is a character vector of length 1 indicating
+    ## the location of the CSV files
+    
+    ## 'pollutant' is a character vector of length 1 indicating
+    ## the name of the pollutant for which we will calculate the
+    ## mean; either "sulfate" or "nitrate".
+    
+    ## 'id' is an integer vector indicating the monitor ID numbers
+    ## to be used
+    
+    ## Return the mean of the pollutant across all monitors list
+    ## in the 'id' vector (ignoring NA values)
+    ## NOTE: Do not round the result!
+    
+    toRet <- numeric(length(id))
+    for(i in id) {
+        if(i<10){
+            prefix <- "00"
+        } else if(i<100) {
+                prefix <- "0"
+        } else {
+            prefix <- ""
+        }
+        fname <- paste(directory,"/",prefix,as.character(i),".csv", sep="")
+        # print(fname)
+        this.data <- read.csv(fname)
+        polvec <- this.data[[pollutant]]
+        toRet[i] <- mean(polvec[!is.na(polvec)]) ## PROBLEM: it's indexing when it should be appending
+    }
+    toRet
+}
